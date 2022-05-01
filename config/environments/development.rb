bb93a9cf3f029748080e415274b033e76b4c2ce8
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-05-01 19:24:13 +0000

    [update]21

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
  config.hosts << "a49c47bd0be04ed48c7918a54f7389ff.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  # config.actuve_job.queue_adapter =:inline
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about'=>'homes#about',as:'about'
   resources :books,only:[:new,:index,:show]
-  resources :users,only:[:show,:edit]
+  resources :users,only:[:index,:show,:edit]
   root to:'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
