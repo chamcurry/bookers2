47459f7b1b716e2e023ee0ba2c002e734542c72e
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-05-03 05:05:34 +0000

    [challenge]27,image tag

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
   def new
+
+  end
+
+  def create
+    @book =Book.new(book_params)
+    @book.user_id =current_user.id
+    @book.save
+    redirect_to book_path(current_user.id)
   end
 
   def index
+    @book =Book.new
+    # @user =User.new
+    # @book =Book.all
+    # @users =User.all
   end
 
   def show
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
