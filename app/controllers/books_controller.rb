d2c14bbc715cfae5b074ea26888af393ee9c808f
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-05-06 10:19:34 +0000

    [finish]33~44

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,11 @@ class BooksController < ApplicationController
   def create
     @book =Book.new(book_params)
     @book.user_id =current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+   if @book.save
+    redirect_to book_path(@book.id),notice: 'You have created book successfully.'
+   else
+     render :books_path
+   end
   end
 
   def index
@@ -26,7 +29,7 @@ class BooksController < ApplicationController
   def update
     @book =Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path
+    redirect_to book_path,notice: 'You have updated book successfully.'
   end
   def destroy
     @book =Book.find(params[:id])
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :user
+# 　validates :title, presence: true
+#   validates :body, presence: true
 end
