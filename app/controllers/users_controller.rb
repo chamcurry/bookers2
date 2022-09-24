41c5b40b5311652890c9956ac769c2076dd01ed8
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-09-24 16:28:47 +0000

    [add]rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def update
    @user =User.find(params[:id])
    if @user.update(user_params)
-     redirect_to user_path,notice: 'You have updated book successfully.'
+     redirect_to user_path,notice: "You have updated book successfully."
    else
     render :edit
    end
